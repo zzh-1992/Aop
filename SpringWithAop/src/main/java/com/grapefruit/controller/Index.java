e8010455a607f89d6f774231d7b59c563d530405
ZhangZhihuang <ezzh1992@icloud.com>
2021-01-10 15:08:58 +0800

    属性注入测试入口

--- SpringWithAop/src/main/java/com/grapefruit/controller/Index.java
+++ SpringWithAop/src/main/java/com/grapefruit/controller/Index.java
@@ -1,6 +1,10 @@
 package com.grapefruit.controller;
 
+import com.grapefruit.run.A;
+import com.grapefruit.run.B;
 import com.grapefruit.run.Run;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 @RestController
 public class Index {
+    @Autowired
+    private A a;
+    @Autowired
+    private B b;
+
+    @Autowired
+    private ApplicationContext context;
+
     @RequestMapping("/ix")
     private String index(){
+        System.out.println(context.getBean("b"));
+        System.out.println(a.getB());
+        System.out.println(b.getA());
         return "Index" + Run.getTimeStr();
     }
 
